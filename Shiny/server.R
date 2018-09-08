58043af0a7cdb71cac179c861f417ca92bb104fd
Mick Sheahan <R3dCobbler@users.noreply.github.com>
2018-09-08 12:43:16 +0200

    Add files via upload

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,22 +1,60 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+ #   server.R
+#   App Name: WordGuess
+#   Author: Mick Sheahan
+#   Date: 7 Sept 2018
 #
 
 library(shiny)
+library(shinythemes)
 
-# Define server logic required to predict next word
+source("global.R")
 
+# Define server logic required to generate next word prediction
 
-function(input, output) {
-  
-  # You can access the value of the widget with input$text, e.g.
-  output$value <- renderPrint({ input$text })
-  
-}
+shinyServer(function(input, output, session) {
+    
+    observe({
+        text <- reactive({input$text})
+        
+        Prediction <- head(unique(predictNextWord(input$text)$word), 3)
+        a1 <<- Prediction[1]
+        a2 <<- Prediction[2]
+        a3 <<- Prediction[3]
+        
+        output$prediction1 <- renderUI({
+            actionButton("button1", label = a1)
+            #HTML("<button id="prediction1" class="shiny-text-output"></button>")
+        })
+        
+        output$prediction2 <- renderUI({
+            actionButton("button2", label = a2)
+        })
+        
+        output$prediction3 <- renderUI({
+            actionButton("button3", label = a3)
+        })
+        
+    })
+    
+    
+    observeEvent(input$button1, {
+        if(input$button1 == 1){
+            name <- paste(input$text, a1)
+            updateTextInput(session, "text", value=name)
+        }
+    })
+    
+    observeEvent(input$button2, {
+        name <- paste(input$text, a2)
+        updateTextInput(session, "text", value=name)
+    })
+    
+    observeEvent(input$button3, {
+        name <- paste(input$text, a3)
+        updateTextInput(session, "text", value=name)
+    })
+    
+})
+
 
 
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -1,44 +1,58 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+#   ui.R
+#   App Name: wordguess
+#   Author: Mick Sheahan
+#   Date: 7 Sept 2018
 #
 
 library(shiny)
 library(shinythemes)
 
-# Define UI for application that predicts the next word
 
-shinyUI(navbarPage("Next word prediction model",
-                   theme = shinytheme("superhero"),
-                   
-fluidPage(
+
+# Define UI for an application to predict suitable next words
+
+shinyUI(
+    fluidPage(
+        theme = shinytheme("superhero"),
+        tags$head(
+            tags$link(rel = "stylesheet", type = "text/css", href = "wordguess.css")
+        ),
+        h1("Word Guess"),
+        br(),
+        h2("A simple predictive text generator"),
+        hr(),
+        fluidRow(
+            column(8, offset = 2,
+                   br(), br(),
+                   h3("Enter your text to see predicted words"),
+                   br(), br()
+                   ),
+                                
+            tags$div(#HTML('<textarea width="100%" id="text" rows="3" cols="30" class="form-control"></textarea>'),
+            tags$textarea(id = 'text', placeholder = 'Type something', rows = 3, class='form-control',""),
+                                           
+            HTML('<script type="text/javascript"> 
+                document.getElementById("text").focus();
+                </script>'),
+                                           
+            HTML("<div id='buttons'>"),
+                uiOutput("prediction1",inline = T),
+                uiOutput("prediction2",inline = T),
+                uiOutput("prediction3",inline = T)),
+            HTML("</div>"),align="center"),
+        
+            h5("Click on one of the words above to add it to your text", align = "center"),
+                                
+            column(3),
+        
+        ## Footer
+            hr(),
+            tags$span(style="color:orange", 
+            tags$footer("Copyright © R3dCobbler 2018", align = "Left"))
+    ))
+
+
+
+
+
   
-  # Copy the line below to make a text input box
-  fluidRow(column(6,
-      textInput("text", label = h3("Type some text here and see the top 3 predicted next words"), 
-                value = "Enter text..."),
-  
-  hr(),
-  fluidRow(column(6, 
-      verbatimTextOutput("value")))
-  
-)
-
-)),
-
-## Footer
-hr(),
-
-
-tags$span(style="color:grey", 
-          
-          tags$footer("@2018-", 
-                      tags$a(
-                        "Mick Sheahan"), 
-                      align = "Left"))
-
-))
